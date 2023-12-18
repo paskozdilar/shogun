ee8fb827c31331619971487610e7a70cffd1c2ae
paskozdilar <paskozdilar@gmail.com>
2023-12-18 13:37:11 -0800

    Add more documentation and .gitignore

--- main.go
+++ main.go
@@ -1,6 +1,97 @@
+/**
+ * Shogun - SSH jumphost manager
+ *
+ * ===
+ *
+ * Nomenclature:
+ *
+ *  _manager_:
+ *      The jumphost SSH server which allows _clients_ to connect to _servers_.
+ *      Access privileges are managed by the admininistrator (TODO: how?).
+ *
+ *  _server_:
+ *        An SSH client that provides access to its TTY to _clients_.
+ *
+ *  _client_:
+ *        An SSH client that gains access to TTY of _servers_.
+ *
+ *  _server-identifier_:
+ *        Fingerprint of the _server_.
+ *        Can be used for both setting _access-permissions_ and starting
+ *        connections.
+ *
+ *  _server-tag_:
+ *        Human-readable string that points to a _server-identifier_.
+ *        Can be used for both setting _access-permissions_ and starting
+ *        connections.
+ *
+ *  _server-group_:
+ *        Human-readable string that refers to a set of _server-identifiers_.
+ *        Can be used for setting _access-permissions_.
+ *
+ *  _client-identifier_:
+ *        Fingerprint of the _client_.
+ *        Can be used for both setting _access-permissions_ and starting
+ *        connections.
+ *
+ *  _client-tag_:
+ *        Human-readable string that points to a _client-identifier_.
+ *        Can be used for setting _access-permissions_.
+ *
+ *  _client-group_:
+ *        Human-readable string that refers to a set of _client-identifiers_.
+ *        Can be used for setting _access-permissions_.
+ *
+ *  _access-permissions_:
+ *        A set of tuples of (_C_, _S_), such that:
+ *        1) _C_ is one of:
+ *          - _client-identifier_
+ *          - _client-tag_
+ *          - _client-group_
+ *        2) _S_ is one of:
+ *          - _server-identifier_
+ *          - _server-tag_
+ *          - _server-group_
+ *
+ * ===
+ *
+ * Functionality:
+ *
+ *  _manager_
+ *
+ * Starts an SSH listener on port 2200.
+ *
+ * When an unknown _server_ connects, it records its fingerprint as a new entry
+ * with no configured permissions. It is up to administrator to allow _clients_
+ * to access the new _server_.
+ *
+ * Each _server_ has an _identifier_ (fingerprint).
+ * Each _server_ has zero or more _tags_.
+ * Each _server_ can belong to zero or more groups.
+ *
+ * A _client_ can be given permission to access a _server_ if, and only if, the
+ * _access-permissions_ set contains a pair (_C_, _S_) such that _C_ refers to
+ * the _client_ and _S_ refers to the _server_
+ *
+ * ===
+ *
+ * Step 1:
+ *  - Create a working SSH client / server implementation that provides a TTY /
+ *    byte stream
+ *
+ * Step 2:
+ *  - ... (Create a reverse server using the byte stream)
+ *
+ * Step 3:
+ *  - ... (Implement...??)
+ */
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"log"
 	"net"
@@ -8,10 +99,17 @@ import (
 	"sync"
 
 	"golang.org/x/crypto/ssh"
-	"golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/term"
 )
 
 func main() {
+	CreateSshKeyPair("id_rsa.pub", "id_rsa")
+}
+
+type AccessPermission struct {
+}
+
+func RunSshServerAndClient() {
 	// Public key authentication is done by comparing
 	// the public key of a received connection
 	// with the entries in the authorized_keys file.
@@ -35,14 +133,16 @@ func main() {
 	// certificate details and handles authentication of ServerConns.
 	config := &ssh.ServerConfig{
 		// Remove to disable password auth.
-		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
-			// Should use constant-time compare (or better, salt+hash) in
-			// a production setting.
-			if c.User() == "testuser" && string(pass) == "tiger" {
-				return nil, nil
-			}
-			return nil, fmt.Errorf("password rejected for %q", c.User())
-		},
+		/*
+			PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
+				// Should use constant-time compare (or better, salt+hash) in
+				// a production setting.
+				if c.User() == "testuser" && string(pass) == "tiger" {
+					return nil, nil
+				}
+				return nil, fmt.Errorf("password rejected for %q", c.User())
+			},
+		*/
 
 		// Remove to disable public key auth.
 		PublicKeyCallback: func(c ssh.ConnMetadata, pubKey ssh.PublicKey) (*ssh.Permissions, error) {
@@ -124,7 +224,7 @@ func main() {
 			wg.Done()
 		}(requests)
 
-		term := terminal.NewTerminal(channel, "> ")
+		term := term.NewTerminal(channel, "> ")
 
 		wg.Add(1)
 		go func() {
@@ -141,4 +241,62 @@ func main() {
 			}
 		}()
 	}
+}
+
+// Make a pair of public and private keys for SSH access.
+// Public key is encoded in the format for inclusion in an OpenSSH
+// authorized_keys file.
+// Private Key generated is PEM encoded.
+func CreateSshKeyPair(pubKeyPath, privateKeyPath string) error {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		return fmt.Errorf("rsa generate key: %w", err)
+	}
+
+	privateKeyFile, err := os.OpenFile(
+		privateKeyPath,
+		os.O_CREATE|os.O_WRONLY,
+		0600,
+	)
+	if err != nil {
+		return fmt.Errorf("open file '%s': %w", privateKeyPath, err)
+	}
+	defer privateKeyFile.Close()
+
+	privateKeyPEM := &pem.Block{
+		Type:  "OPENSSH PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	}
+	if err := pem.Encode(privateKeyFile, privateKeyPEM); err != nil {
+		return fmt.Errorf("pem encode to '%s': %w", privateKeyPath, err)
+	}
+
+	pub, err := ssh.NewPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("rsa to ssh public key: %w", err)
+	}
+	err = os.WriteFile(
+		pubKeyPath,
+		ssh.MarshalAuthorizedKey(pub),
+		0644,
+	)
+	if err != nil {
+		return fmt.Errorf("ssh public key write to '%s': %w", pubKeyPath, err)
+	}
+	return nil
+}
+
+/**
+ * This function (attempts to) atomically writes data to a file specified by
+ * the filename. It will probably work on Linux.
+ */
+func atomicWrite(filename string, data []byte) error {
+	tempFilename := filename + ".temp"
+	if err := os.WriteFile(tempFilename, data, 0644); err != nil {
+		return err
+	}
+	if err := os.Rename(tempFilename, filename); err != nil {
+		return err
+	}
+	return nil
 }
